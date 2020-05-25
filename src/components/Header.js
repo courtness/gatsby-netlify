25205617ebdf619a777242c0d6b5620554fb9c58
Daniel Courtness <danielcourtness@gmail.com>
2020-05-26 09:27:18 +1000

    More CSS updates, fix build scripts

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ class HeaderComponent extends Component {
       <header
         className={`header ${appContext.cartActive ? ` cart-active` : ``} ${
           appContext.menuActive ? ` menu-active` : ``
-        } transition-transform w-full fixed top-0 right-0 left-0 z-30 py-2 bg-white border-b-black`}
+        } transition-transform w-full fixed top-0 right-0 left-0 z-30 py-2 border-b-black`}
       >
         <nav className="grid">
           <div className="grid-end-12 flex items-center justify-between">
