9557b60a5f23c9d4a6c85740c3c17fa8125da51f
courtness <danielcourtness@gmail.com>
2020-11-24 13:52:27 +0900

    Improved ScreenHeight

--- src/components/ScreenHeight.js
+++ src/components/ScreenHeight.js
@@ -1,26 +1,24 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 import { DocumentContext } from "~context/DocumentContext";
 
 const ScreenHeight = ({ children, className }) => {
   const { windowHeight, windowWidth } = useContext(DocumentContext);
-  const [height, setHeight] = useState(`100vh`);
 
   useEffect(() => {
-    if (typeof window === `undefined`) {
+    if (typeof window === `undefined` || typeof document === `undefined`) {
       return;
     }
 
-    setHeight(document?.documentElement?.clientHeight || window.innerHeight);
+    document.documentElement.style.setProperty(
+      `--vh`,
+      `${windowHeight * 0.01}px`
+    );
   }, [windowHeight, windowWidth]);
 
   //
 
-  return (
-    <section className={className} style={{ height }}>
-      {children}
-    </section>
-  );
+  return <section className={`screen-height ${className}`}>{children}</section>;
 };
 
 ScreenHeight.propTypes = {
